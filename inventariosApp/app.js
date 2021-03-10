3c55afd62ccb16f4197412c6caa2de1bd3351692
Guillermo Ballesteros <babiloneos@gmail.com>
2021-03-10 10:56:02 -0800

    Clase 10 Marzo
    Funciona el login por POST, y se puso al corriente el codigo con la clase anterior.

--- inventariosApp/app.js
+++ inventariosApp/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -17,6 +19,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// sessions
+app.use(session({
+  resave: false,  //persistencia de sesion a traves de la app
+  saveUninitialized: true,
+  secret:'notelodigo'
+}))
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 //bootstrap
